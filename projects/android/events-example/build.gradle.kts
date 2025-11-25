725acac3af4a252df30f3a80d031d385dc4c4c3d
Jonny Paton <jonathan.r.paton@googlemail.com>
2025-11-25 13:05:11 +0000

    Separate module for SFML. Add module for events example. Remove default android project guff

--- projects/android/events-example/build.gradle.kts
+++ projects/android/events-example/build.gradle.kts
@@ -5,15 +5,17 @@ plugins {
 }
 
 android {
-    namespace = "com.sfml.window_example"
+    namespace = "com.sfml.events_example"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.sfml.window_example"
+        applicationId = "com.sfml.events_example"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
--- projects/android/settings.gradle.kts
+++ projects/android/settings.gradle.kts
@@ -21,3 +21,5 @@ dependencyResolutionManagement {
 
 rootProject.name = "SFML"
 include(":window-example")
+include(":events-example")
+include(":SFML")
